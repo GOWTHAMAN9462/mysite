1353e33fa15420171fce61377f9e6e04c3bffe46
Gowthaman K S <gowthaman.cs20@bitsathy.ac.in>
2021-12-23 19:20:10 +0530

    update

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from scraper import app
-from dotenv import load_dotenv
+from flask import Flask
 
+from dotenv import load_dotenv
 load_dotenv('.env')
+
+from os import environ
+
+app = Flask(__name__)
+
+from scraper import routes
 
 if __name__ == '__main__':
     app.run()
--- scraper/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from os import environ
-
-app = Flask(__name__)
-
-from scraper import routes
